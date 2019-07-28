b6f57163a1039abf24259dcaed1834cff95b050a
michaelcordero <michaelpetercordero@gmail.com>
2019-07-27 20:08:54 -0500

    fixed test

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -18,11 +18,11 @@ class Tests: XCTestCase {
         XCTAssert(true, "Pass")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure() {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
