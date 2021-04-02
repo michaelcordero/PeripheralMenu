e5ebc89af342b61e058a6498c5337ce1b6096900
michaelcordero <michaelpetercordero@gmail.com>
2021-04-01 23:23:28 -0400

    Removed CocoaPods

--- Example/Pods/Target Support Files/PeripheralMenu/PeripheralMenu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PeripheralMenu : NSObject
-@end
-@implementation PodsDummy_PeripheralMenu
-@end
--- Example/Pods/Target Support Files/Pods-PeripheralMenu_Tests/Pods-PeripheralMenu_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PeripheralMenu_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_PeripheralMenu_Tests
-@end
