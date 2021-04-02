f5c4a17eb83e1383529a934e1e3f6545c01a4ca6
michaelcordero <michaelpetercordero@gmail.com>
2021-04-01 23:09:51 -0400

    Removed CocoaPods

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import PeripheralMenuTests
+
+var tests = [XCTestCaseEntry]()
+tests += PeripheralMenuTests.allTests()
+XCTMain(tests)
---
+++ Tests/PeripheralMenuTests/PeripheralMenuTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import PeripheralMenu
+
+final class PeripheralMenuTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PeripheralMenu().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
